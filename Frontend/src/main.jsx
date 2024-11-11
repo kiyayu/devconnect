042391c4dcd3d02bd1c47b7925b866a3efdc6017
kiyayu <142785976+kiyayu@users.noreply.github.com>
2024-11-11 15:15:19 +0300

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,9 +5,7 @@ import App from './Q/App'
 import './index.css'
 import { ToastContainer } from 'react-toastify'; // Import the toast container
 import 'react-toastify/dist/ReactToastify.css';
- //BY0iErycYClxWnIQ
-//mongodb+srv://tarikufirdu:BY0iErycYClxWnIQ @devconnect.a99pu.mongodb.net/?retryWrites=true&w=majority&appName=devconnect
-//mongodb+srv://tesfa:<db_password>@devconnect.a99pu.mongodb.net/?retryWrites=true&w=majority&appName=devconnect
+ 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider  >
